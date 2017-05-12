4222b7e54704dbae4a2cb418643d625ec4884448
youngwan <tiny657@coupang.com>
2017-05-13 03:35:45 +0900

    initialized project

---
+++ src/main/java/com/reddit/vote/VoteApplication.java
@@ -0,0 +1,12 @@
+package com.reddit.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApplication.class, args);
+	}
+}
